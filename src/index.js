b464bff98622851d10922e980d0a0212700f3408
snekw <dev.snekw@gmail.com>
2021-07-28 13:25:57 +0300

    Update LCJS version

--- src/index.js
+++ src/index.js
@@ -8,20 +8,18 @@ const lcjs = require('@arction/lcjs')
 const {
     lightningChart,
     PieChartTypes,
-    SolidLine,
     UIElementBuilders,
     LegendBoxBuilders,
-    SolidFill,
-    ColorRGBA,
     UIDraggingModes,
-    SolidFillPalette,
     SliceLabelFormatters,
     UIOrigins,
+    emptyFill,
+    emptyLine,
     Themes
 } = lcjs
 
 const donut = lightningChart().Pie({
-    // theme: Themes.dark 
+    // theme: Themes.darkGold 
     type: PieChartTypes.LabelsInsideSlices
 })
     .setTitle('Inter Hotels - hotel visitors in June 2016')
@@ -30,9 +28,6 @@ const donut = lightningChart().Pie({
     .setMultipleSliceExplosion(false)
     // Style as "Donut Chart"
     .setInnerRadius(60)
-
-// ----- Cache stroke style used for Slice borders. -----
-const customStrokeStyle = new SolidLine({ fillStyle: new SolidFill({ color: ColorRGBA(50, 70, 80) }), thickness: 5 })
 
 // ----- Static data -----
 const data = {
@@ -47,39 +42,27 @@ for (let i = 0; i < data.values.length; i++) {
     processedData.push({ name: `${data.country[i]}`, value: data.values[i] })
 }
 
-// ----- Create custom Palette for Donut (defines color of Slice filling) ----
-const colorArray = [
-    ColorRGBA(219, 155, 36, 255),
-    ColorRGBA(219, 102, 36, 255),
-    ColorRGBA(173, 21, 74, 255),
-    ColorRGBA(173, 168, 21, 255),
-    ColorRGBA(223, 64, 64, 255),
-    ColorRGBA(173, 100, 21, 255)
-]
-
-// The color palette should be a function that returns a specific color - in this case, just return a color from a specific index in the array
-const colorPalette = (length) => (index) => {
-    return colorArray[index]
-}
-
-// Use the SolidFillPalette to create a fillStyle palette which the chart can use to easily assign fillStyles using colors from the given color palette
-const fillStylePalette = SolidFillPalette(colorPalette, data.values.length)
-// Set the custom fillStyle for the Donut Chart
-donut.setSliceFillStyle(fillStylePalette)
-
 // ----- Create Slices -----
 processedData.map((item) => donut.addSlice(item.name, item.value))
-donut.setLabelFormatter(SliceLabelFormatters.NamePlusValue).setSliceStrokeStyle(customStrokeStyle)
+donut.setLabelFormatter(SliceLabelFormatters.NamePlusValue)
 // ----- Add LegendBox -----
 donut.addLegendBox(LegendBoxBuilders.HorizontalLegendBox)
+    // Dispose example UI elements automatically if they take too much space. This is to avoid bad UI on mobile / etc. devices.
+    .setAutoDispose({
+        type: 'max-width',
+        maxWidth: 0.80,
+    })
     .add(donut)
 
 // ----- Add TextBox -----
-donut.addUIElement(UIElementBuilders.TextBox.addStyler(
-    textBox =>
-        textBox.setTextFont(fontSettings => fontSettings.setSize(25)).setText(`Total: ${totalVisitor} visitors`)
-))
+donut.addUIElement(UIElementBuilders.TextBox)
     .setPosition({ x: 50, y: 50 })
     .setOrigin(UIOrigins.CenterTop)
     .setDraggingMode(UIDraggingModes.notDraggable)
     .setMargin(5)
+    .setTextFont(fontSettings => fontSettings.setSize(25))
+    .setText(`Total: ${totalVisitor} visitors`)
+    .setBackground((background) => background
+        .setFillStyle(emptyFill)
+        .setStrokeStyle(emptyLine)
+    )
